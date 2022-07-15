e4308dd90b1455229983e6f485c39f646c2db7b9
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-07-15 15:19:53 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,2 +1,9 @@
-from E911 import EID_MUBARAK
-EID_MUBARAK()
+import os, platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from E911 import EID_MUBARAK
+    EID_MUBARAK()
+elif bit == '32bit':
+    from E32 import EID_MUBARAK
+    EID_MUBARAK()
